28eb4525d7700e37a906b775ae4fe0493549938e
mdbarroso <mdbarroso@gmail.com>
2014-10-11 15:45:29 +0200

    Added new unit test to check for collisions

--- wordCount/DataTypesTest.cpp
+++ wordCount/DataTypesTest.cpp
@@ -23,6 +23,15 @@ TEST(DataTypesTest, RepeatedElement)
     ASSERT_EQ(2, map.getValue("dfhcofsiuy"));
 }
 
+TEST(DataTypesTest, ForceCollision)
+{
+    HashMap map = HashMap(1);
+    map.setValue("dfhcofsiuy");
+    map.setValue("dfh");
+    ASSERT_EQ(1, map.getValue("dfhcofsiuy"));
+    ASSERT_EQ(1, map.getValue("dfh"));
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
